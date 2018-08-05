c80b50cdfe1dd5bd769fbd24631e8c98c3fe1be0
Mike Chabot <mike.chabot@live.com>
2018-08-05 18:17:15 -0400

    Adding comments

--- spoof.py
+++ spoof.py
@@ -1,7 +1,7 @@
 import sys
 from setup import init
 
-init()
+init()  # Add packages to PYTHONPATH
 
 from args import config
 from commands import cli, wizard
@@ -9,17 +9,16 @@ from commands import cli, wizard
 if __name__ == '__main__':
     arg_length = len(sys.argv)
     if arg_length == 1:
-        config.parser.print_help()
+        config.parser.print_help()      # Print help
         exit(1)
     elif arg_length == 2:
         if 'wizard' in sys.argv:
-            wizard.run()
+            wizard.run()                # Run wizard
         elif 'cli' in sys.argv:
-            config.cli.print_help()
+            config.cli.print_help()     # Print CLI help
             exit(1)
         else:
-            config.parser.parse_args() # generate parser warning messages
+            config.parser.parse_args()  # Generate parser warning messages
     else:
         args = config.parser.parse_args()
-        print(args)
-        cli.run(args)
+        cli.run(args)                   # Run CLI
